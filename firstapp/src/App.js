eaa10394bd08f9b22e6b30f77f6120a38b3ef8d8
Shiv-96 <kshiv.dot@gmail.com>
2023-11-01 15:34:20 +0530

    First react App

---
+++ firstapp/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Shailanshu</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        consequatur dolore assumenda! Mollitia amet maxime eaque asperiores
+        ipsam sequi cumque neque ratione quis ad, possimus rerum maiores atque
+        eius sunt.
+      </p>
+    </div>
+  );
+};
+
+export default App;
